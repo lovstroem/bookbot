6a147156d8c336de5c81bad998bf1bd3828ea35e
Lovstroem <lovstroem@gmail.com>
2025-05-07 21:20:05 +0200

    main prints number of words in frankenstein

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+# Reads a file and returns the text as a string
 def get_book_text(book):
     with open (book) as f:
         book_content = f.read()
     return book_content
 
+# Accepts a text as a string and return the number of words used
+def number_of_words(text):
+    count = 0
+    for word in text.split():
+        count += 1
+    return count
+
+# Calls get_book_text and number_of_words to get the number of words in the frankenstein text
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    words_count = number_of_words(get_book_text("books/frankenstein.txt"))
+    print(f"{words_count} words found in the document")
+
 
 main()
