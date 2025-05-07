83becc33b207d13bf7c869a0b264e0f34665bdba
Lovstroem <lovstroem@gmail.com>
2025-05-07 20:48:14 +0200

    main reads and prints frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(book):
+    with open (book) as f:
+        book_content = f.read()
+    return book_content
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
